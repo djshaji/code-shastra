00b878e46d7ad8c48d3eb52fd063ef2ad77161c6
DJ Shaji <saksham@shaji.in>
2020-02-02 20:09:41 +0530

    first commit

---
+++ mail/contact_me.php
@@ -0,0 +1,52 @@
+<?php
+ require_once "Mail.php";
+ 
+ $from = "Sandra Sender <sender@example.com>";
+ $to = "Ramona Recipient <recipient@example.com>";
+ $subject = "Hi!";
+ $body = "Hi,\n\nHow are you?";
+ 
+ $host = "mail.shajikhan.in";
+ $username = "code@shaji.in";
+ $password = "hRuaNtw#w3oe[3,;]*";
+ 
+ $headers = array ('From' => $from,
+   'To' => $to,
+   'Subject' => $subject);
+ $smtp = Mail::factory('smtp',
+   array ('host' => $host,
+     'auth' => true,
+     'username' => $username,
+     'password' => $password));
+ 
+ ?>
+ <?php
+// Check for empty fields
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+// Create the email and send the message
+$to = "yourname@yourdomain.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "Website Contact Form:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header .= "Reply-To: $email";	
+
+// if(!mail($to, $subject, $body, $header))
+//   http_response_code(500);
+$mail = $smtp->send($to, $headers, $body);
+ 
+ if (PEAR::isError($mail)) {
+   echo("<p>" . $mail->getMessage() . "</p>");
+  } else {
+   echo("<p>Message successfully sent!</p>");
+  }
+
+?>
